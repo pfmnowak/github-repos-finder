65b4e7ece36cb60763308019463fcd8b30824451
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2022-12-03 22:53:07 +0100

    Style Repo items

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import InlineLink from './InlineLink';
 import classes from './Repo.module.scss';
 
 const Repo = props => {
 	return (
 		<div className={classes.repo}>
-			<div className={classes.repo__name}>{props.name}</div>
+			<div className={classes.repo__name}>
+				<InlineLink link={props.link} title="Link to the GitHub repository">
+					{props.name}
+				</InlineLink>
+			</div>
 			<div className={classes.repo__lang}>
 				{props.language ? props.language : '-'}
 			</div>
 			<div className={classes.repo__stars}>{props.stars}⭐</div>
-			<a className={classes.repo__link} href={props.link} target="_blank">
-				link
-			</a>
 		</div>
 	);
 };
